36376fad86501e1349d1d89b90caa004f27e1637
sumit <thesumitkolhe@gmail.com>
2020-07-22 18:26:47 +0530

    Wrapped viper functions

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,63 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+	"github.com/kintohub/kinto-cli-go/internal/utils"
+	"github.com/spf13/viper"
+)
 
 const (
-	Version         = "v0.1"
+	Version = "v0.1"
 	KintoMasterHost = "master.vegeta.kintohub.net:443"
 )
 
-func SetAuthToken(token string)  {
+
+
+func AddConfigPath(path string) {
+	viper.AddConfigPath(path)
+}
+
+func SetConfigName(configName string) {
+	viper.SetConfigName(configName)
+}
+
+func SetConfigType(configType string) {
+	viper.SetConfigType(configType)
+}
+
+func AutomaticEnv() {
+	viper.AutomaticEnv()
+}
+
+func CreateConfig(path string, configName string) {
+	if err := viper.ReadInConfig(); err != nil {
+		// Create new config file
+		err := viper.WriteConfigAs(fmt.Sprintf("%s/%s",
+			path,
+			configName,
+		))
+		if err != nil {
+			utils.TerminateWithError(err)
+		}
+	}
+}
+
+func WriteConfig() {
+	err := viper.WriteConfig()
+	if err != nil {
+		utils.TerminateWithError(err)
+	}
+}
+
+func GetString(string string) string {
+	str := viper.GetString(string)
+	return str
+}
+
+func SetAuthToken(token string) {
 	viper.Set("authToken", token)
 }
 
-func SetEmail(email string)  {
+func SetEmail(email string) {
 	viper.Set("email", email)
 }
